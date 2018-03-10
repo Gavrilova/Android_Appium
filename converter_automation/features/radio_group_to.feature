cb8146ed1d488d3813046b214daf07d2b965ee79
Gavrilova <gavrilova.irina@gmail.com>
2018-03-10 14:49:50 -0600

    Report file changed
    Improved cucumber.yml file:
    Adde new tag to cucumber.yml.

---
+++ converter_automation/features/radio_group_to.feature
@@ -0,0 +1,16 @@
+@groupTo
+  Feature: User is able to switch between the group_to radio buttons in the "To" right column
+
+    Background:
+      Given I click on Got it button
+      And I ground on Area screen
+      And I see "1.0" in the From text field
+
+      #@wip
+      Scenario:
+        Then I see text in To header
+        Then I find index checked radio button
+  #      Then I select the next one radio button in the next column
+  #      And I see "<string>" in To header
+
+
---
+++ converter_automation/features/step_definitions/radio_group_to.rb.rb
@@ -0,0 +1,48 @@
+And(/^I see "([^"]*)" in the From text field$/) do |value|
+  if find_element(id: "header_value_from").text != "#{value}"
+    raise("#{value} in the From field is differ than 1.0 ")
+  end
+end
+
+
+Then(/^I find index checked radio button$/) do
+
+  column_to = counts_right_column_radio_buttons()
+
+  column_to.delete_at(1)
+  3.times {
+
+    i = rand(column_to.length - 1)
+    elem = column_to[i]
+ #   puts("column_to.index(#{i}) = #{elem}")
+    select_radio_button(elem)
+    if find_element(id: "header_text_unit_to").text == elem
+#      puts("Header:", find_element(id: "header_text_unit_to").text)
+    else
+      raise ("Header #{find_element(id: "header_text_unit_to").text}  should be #{elem}  !")
+    end
+
+
+    if find_element(id: "header_text_unit_to").text == ""
+      raise("There is no results after conversions!")
+    end
+
+    conversion
+
+    column_to.delete_at(i)
+ #   puts("Remain radio button to test:", column_to.size)
+    column_to.each do |v|
+ #     puts v
+    end
+  }
+end
+
+
+Then(/^I select the next one radio button in the next column$/) do
+  put("next step to code will continue")
+end
+
+Then(/^I see text in To header$/) do
+  header_to_text = find_element(id: "header_text_unit_to").text
+#  puts("Header To text: " + header_to_text)
+end
